4b8752bb59f1c969edc1597f34978f3d7271500f
YasirZardari <yasirzardari@gmail.com>
2025-01-03 16:16:35 +0000

    Add command to get due trains by station

--- main.py
+++ main.py
@@ -5,12 +5,26 @@ import xml.etree.ElementTree as ET
 app = typer.Typer()
 
 @app.command()
-def hello():
-    print("Hello")
+def getListOfStations():
     response = requests.get('https://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML')
     root = ET.fromstring(response.content)
     for child in root:
-        print(child.tag, child.attrib)
+        print(child[0].text)
 
-if __name__ == "__main__":
+@app.command()
+def getTrainsByStation(station: str):
+    response = requests.get('https://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc=' + station)
+    root = ET.fromstring(response.content)
+    for child in root:
+        namespace = {'ns': 'http://api.irishrail.ie/realtime/'}
+        destination = child.find('ns:Destination', namespace).text
+        dueIn = child.find('ns:Duein', namespace).text
+        late = child.find('ns:Late', namespace).text
+        if destination != station:
+            if late == '0':
+                print(f"{destination} is due in {dueIn} mins")
+            else:
+                print(f"{destination} is due in {dueIn} mins (late by {late} mins)")
+
+if __name__ == "__main__":  
     app()
